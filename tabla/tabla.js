a94dab13271aa4b17e25b88c119a0c6fa6dd705d
Gissela Sobenes <gisselasobenes@gmail.com>
2024-05-06 23:55:35 +0200

    funciones de editar, eliminar y actualizar datos creada

--- tabla/tabla.js
+++ tabla/tabla.js
@@ -28,6 +28,9 @@ function ValidateForm() {
     if (phone == "") {
         alert('Campo obligatorio!');
         return false;
+    }else if (!/^\d+$/.test(phone)) {
+        alert('El número de teléfono no es válido.');
+        return false;
     }
 
     if (country == "") {
@@ -55,15 +58,15 @@ function ReadData() {
     let html = "";
 
     listPeople.forEach(function(element, index) {
-        html += "<tr>";
-        html += "<td>" + element.name + "</td>";
-        html += "<td>" + element.lastName + "</td>";
-        html += "<td>" + element.email + "</td>";
-        html += "<td>" + element.phone + "</td>";
-        html += "<td>" + element.country + "</td>";
-        html += "<td>" + element.city + "</td>";
-        html += '<td><button onclick="deleteData('+ index +') class="btn btn-danger">Eliminar</button></td>';
-        html += '<td><button onclick="editData('+ index +') class="btn btn-warning">Editar</button></td>';
+        html += "<tr id='row_"+index+"'>";
+        html += "<td><input type='text' class='form-control' value='" + element.name + "' id='editName_"+index+"' disabled></td>";
+        html += "<td><input type='text' class='form-control' value='" + element.lastName + "' id='editLastName_"+index+"' disabled></td>";
+        html += "<td><input type='email' class='form-control' value='" + element.email + "' id='editEmail_"+index+"' disabled></td>";
+        html += "<td><input type='tel' class='form-control' value='" + element.phone + "' id='editPhone_"+index+"' disabled></td>";
+        html += "<td><input type='text' class='form-control' value='" + element.country + "' id='editCountry_"+index+"' disabled></td>";
+        html += "<td><input type='text' class='form-control' value='" + element.city + "' id='editCity_"+index+"' disabled></td>";
+        html += "<td><button onclick='editData("+ index +")' class='btn btn-warning'>Editar</button></td>";
+        html += "<td><button onclick='deleteData("+ index +")' class='btn btn-danger'>Eliminar</button></td>";
         html += "</tr>";
     });
 
@@ -71,6 +74,53 @@ function ReadData() {
 }
 
 window.onload = ReadData;
+
+function editData(index) {
+    let row = document.getElementById('row_'+index);
+    let inputs = row.querySelectorAll('input');
+
+    inputs.forEach(input => {
+        input.disabled = false;
+    });
+
+    let editButton = row.querySelector('button');
+    editButton.textContent = 'Guardar';
+    editButton.classList.remove('btn-warning');
+    editButton.classList.add('btn-success');
+    editButton.setAttribute('onclick', 'updateData('+index+')');
+}
+
+function updateData(index) {
+    let listPeople;
+
+    if (localStorage.getItem('listPeople') == null) {
+        listPeople = [];
+    } else {
+        listPeople = JSON.parse(localStorage.getItem('listPeople'));
+    }
+
+    let row = document.getElementById('row_'+index);
+    let inputs = row.querySelectorAll('input');
+
+    listPeople[index].name = inputs[0].value;
+    listPeople[index].lastName = inputs[1].value;
+    listPeople[index].email = inputs[2].value;
+    listPeople[index].phone = inputs[3].value;
+    listPeople[index].country = inputs[4].value;
+    listPeople[index].city = inputs[5].value;
+
+    localStorage.setItem('listPeople', JSON.stringify(listPeople));
+
+    inputs.forEach(input => {
+        input.disabled = true;
+    });
+
+    let editButton = row.querySelector('button');
+    editButton.textContent = 'Editar';
+    editButton.classList.remove('btn-success');
+    editButton.classList.add('btn-warning');
+    editButton.setAttribute('onclick', 'editData('+index+')');
+}
 
 function AddData(){
     if(ValidateForm() == true) {
@@ -108,7 +158,22 @@ function AddData(){
         document.getElementById('inputPhone').value = "";
         document.getElementById('inputCountry').value = "";
         document.getElementById('inputCity').value = "";
-
-        window.location.href = "./tabla.html";
     }
 }
+
+
+function deleteData(index) {
+    let listPeople;
+
+    if (localStorage.getItem('listPeople') == null) {
+        listPeople = [];
+    } else {
+        listPeople = JSON.parse(localStorage.getItem('listPeople'));
+    }
+
+    listPeople.splice(index, 1);
+    localStorage.setItem('listPeople', JSON.stringify(listPeople));
+
+    ReadData();
+}
+
