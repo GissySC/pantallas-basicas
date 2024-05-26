40f207a75a2515bb9e9d9e12f9a4e90a3b864480
Gissela Sobenes <gisselasobenes@gmail.com>
2024-05-26 22:39:04 +0200

    table responsiva, vertical version movil

--- tabla/tabla.js
+++ tabla/tabla.js
@@ -97,16 +97,18 @@ function ReadData() {
 
     listPeople.forEach(function(element, index) {
         html += "<tr id='row_"+index+"'>";
-        html += "<td class='table-dark'><input type='text' class='form-control' value='" + element.name + "' id='editName_"+index+"' disabled></td>";
-        html += "<td class='table-dark'><input type='text' class='form-control' value='" + element.lastName + "' id='editLastName_"+index+"' disabled></td>";
-        html += "<td class='table-dark'><input type='email' class='form-control' value='" + element.email + "' id='editEmail_"+index+"' disabled></td>";
-        html += "<td class='table-dark'><input type='tel' class='form-control' value='" + element.phone + "' id='editPhone_"+index+"' disabled></td>";
-        html += "<td class='table-dark'><input type='text' class='form-control' value='" + element.country + "' id='editCountry_"+index+"' disabled></td>";
-        html += "<td class='table-dark'><input type='text' class='form-control' value='" + element.city + "' id='editCity_"+index+"' disabled></td>";
-        html += "<td class='table-dark'><button onclick='editData("+ index +")' class='btn btn-warning'>Editar</button></td>";
-        html += "<td class='table-dark'><button onclick='deleteData("+ index +")' class='btn btn-danger'>Eliminar</button></td>";
-        html += "<td class='table-dark'><button onclick='viewUser("+ index +")' class='btn btn-primary'>Ver</button></td>";
-
+        html += "<td class='table-dark data-label'><input type='text' class='form-control' value='" + element.name + "' id='editName_"+index+"' disabled></td>";
+        html += "<td class='table-dark data-label'><input type='text' class='form-control' value='" + element.lastName + "' id='editLastName_"+index+"' disabled></td>";
+        html += "<td class='table-dark data-label'><input type='email' class='form-control' value='" + element.email + "' id='editEmail_"+index+"' disabled></td>";
+        html += "<td class='table-dark data-label'><input type='tel' class='form-control' value='" + element.phone + "' id='editPhone_"+index+"' disabled></td>";
+        html += "<td class='table-dark data-label'><input type='text' class='form-control' value='" + element.country + "' id='editCountry_"+index+"' disabled></td>";
+        html += "<td class='table-dark data-label'><input type='text' class='form-control' value='" + element.city + "' id='editCity_"+index+"' disabled></td>";
+        html += "<td class='table-dark'>";
+        html += "<div class='actions'>";
+        html += "<button onclick='editData(" + index + ")' class='btn btn-warning'>Editar</button>";
+        html += "<button onclick='deleteData(" + index + ")' class='btn btn-danger'>Eliminar</button>";
+        html += "<button onclick='viewUser(" + index + ")' class='btn btn-primary'>Ver</button>";
+        html += "</div></td>";
         html += "</tr>";
     });
 
